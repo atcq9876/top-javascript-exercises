07cbe37dfbd99dc1631f3c14616b37df34d0b9b9
Andy <ajscottlewis@gmail.com>
2022-10-14 17:44:28 +0100

    Successfully complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = string;
+    let i = 1;
+    if (num === 0) {
+        return '';
+    }
+    else if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        while (i < num) {
+            newString = newString + string;
+            i++;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
